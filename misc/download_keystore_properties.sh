b55139962708d248a87a420de8bafa3b8070b90a
Tinashe Mzondiwa <tmzondiwa@jhb.dvt.co.za>
2018-07-23 18:52:10 +0200

    Update yml file

--- misc/download_keystore_properties.sh
+++
@@ -1,9 +0,0 @@
-# use curl to download a keystore from $KEYSTORE_URI, if set,
-# to the path/filename set in $KEYSTORE_PROP.
-if [[ $KEYSTORE_PROP && ${KEYSTORE_PROP} && $KEYSTORE_PROP_URI && ${KEYSTORE_PROP_URI} ]]
-then
-    echo "Keystore properties detected - downloading..."
-    curl -L -o ${KEYSTORE_PROP} ${KEYSTORE_PROP_URI}
-else
-    echo "Keystore properties uri not set.  .APK artifact will not be signed."
-fi
